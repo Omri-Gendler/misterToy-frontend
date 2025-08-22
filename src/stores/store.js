4fc8627f6a124f28f093c470bce1fee133968650
omri-gendler <omrig999@gmail.com>
2025-08-22 09:08:26 +0300

    feat: add toy management actions and update AppReducer for state handling

--- src/stores/store.js
+++ src/stores/store.js
@@ -2,6 +2,10 @@ import store from 'redux'
 
 
 export const SET_TOYS = 'SET_TOYS'
+export const ADD_TOY = 'ADD_TOY'
+export const REMOVE_TOY = 'REMOVE_TOY'
+export const UPDATE_TOY = 'UPDATE_TOY'
+export const SET_FILTER = 'SET_FILTER'
 
 const initialState = {
     toys: [],
@@ -13,6 +17,17 @@ export function AppReducer(state = initialState, cmd = {}) {
     switch (cmd.type) {
         case 'SET_TOYS':
             return { ...state, toys: cmd.toys }
+        case 'ADD_TOY':
+            return { ...state, toys: [...state.toys, cmd.toy] }
+        case 'REMOVE_TOY':
+            return { ...state, toys: state.toys.filter(toy => toy._id !== cmd.toyId) }
+        case 'UPDATE_TOY':
+            return {
+                ...state,
+                toys: state.toys.map(toy => toy._id === cmd.toy._id ? cmd.toy : toy)
+            }
+        case 'SET_FILTER':
+            return { ...state, filterBy: cmd.filterBy }
         default:
             return state
     }
