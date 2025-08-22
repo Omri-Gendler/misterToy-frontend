c141571ee307678e827040f1f0f4d69fd85c3447
omri-gendler <omrig999@gmail.com>
2025-08-22 09:12:42 +0300

    refactor: reorganize toy actions for improved readability and maintainability

--- src/actions/toy.actions.js
+++ src/actions/toy.actions.js
@@ -1,4 +1,5 @@
 import { storageService } from './async-storage.service.js'
+import { SET_TOYS, ADD_TOY, REMOVE_TOY, UPDATE_TOY, SET_FILTER } from '../stores/store.js'
 
 
 export function loadToys(filterBy) {
@@ -7,4 +8,38 @@ export function loadToys(filterBy) {
         .catch(err => {
             console.error('Error loading toys:', err)
         })
+}
+
+export function removeToy(toyId) {
+    storageService.remove(toyId)
+        .then(() => {
+            store.dispatch({ type: REMOVE_TOY, toyId })
+        })
+        .catch(err => {
+            console.error('Error removing toy:', err)
+        })
+}
+
+export function updateToy(toy) {
+    storageService.put(toy)
+        .then(() => {
+            store.dispatch({ type: UPDATE_TOY, toy })
+        })
+        .catch(err => {
+            console.error('Error updating toy:', err)
+        })
+}
+
+export function addToy(toy) {
+    storageService.post(toy)
+        .then((newToy) => {
+            store.dispatch({ type: ADD_TOY, toy: newToy })
+        })
+        .catch(err => {
+            console.error('Error adding toy:', err)
+        })
+}
+
+export function setFilter(filterBy) {
+    store.dispatch({ type: SET_FILTER, filterBy })
 }
