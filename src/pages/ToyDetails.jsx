779779c9965c3a3362e7a73326b0bcd34f7033e0
omri-gendler <omrig999@gmail.com>
2025-08-23 22:44:40 +0300

    refactor: remove deprecated toy actions and update loadToys function to use toyService

--- src/pages/ToyDetails.jsx
+++ src/pages/ToyDetails.jsx
@@ -4,6 +4,9 @@ import { useParams } from "react-router-dom"
 import { AppHeader } from "../cmps/AppHeader.jsx"
 // import { loadToys } from "../stores/toy.actions.js"
 import '../assets/style/cmps/TodoDetails.css' // Import the CSS
+import { toyService } from "../services/toy.service.js"
+
+const TOY_KEY = 'toyDB'
 
 export function ToyDetails() {
     const [toy, setToy] = useState(null)
@@ -14,11 +17,10 @@ export function ToyDetails() {
 
 
     function loadToys() {
-        storageService.query('toyDB')
+        toyService.query(TOY_KEY)
             .then(toys => {
+                setToy(toys)
                 console.log('Loaded toys:', toys)
-                setToy(toys[0])
-                console.log('Loaded toy:', toys[0])
             })
     }
 
--- src/stores/toy.actions.js
+++ src/stores/toy.actions.js
@@ -1,19 +1,45 @@
-import { SET_TOYS, SET_IS_LOADING } from "../reducers/toy.reducer"
-import { store } from '../stores/store.js'
-import {toyService} from '../services/toy.service.js'
+import { storageService } from './async-storage.service.js'
+import { SET_TOYS, ADD_TOY, REMOVE_TOY, UPDATE_TOY, SET_FILTER } from '../stores/store.js'
 
-export function loadToys() {
-    const filterBy = store.getState().toyModule.filterBy
-    store.dispatch({ type: SET_IS_LOADING, isLoading: true })
-    return toyService.query(filterBy)
-        .then(toys => {
-            store.dispatch({ type: SET_TOYS, toys })
+
+export function loadToys(filterBy) {
+    storageService.query(filterBy)
+        .then(toys => store.dispatch({ type: SET_TOYS, toys }))
+        .catch(err => {
+            console.error('Error loading toys:', err)
+        })
+}
+
+export function removeToy(toyId) {
+    storageService.remove(toyId)
+        .then(() => {
+            store.dispatch({ type: REMOVE_TOY, toyId })
         })
         .catch(err => {
-            console.log('toy action -> Cannot load toys', err)
-            throw err
+            console.error('Error removing toy:', err)
         })
-        .finally(() => {
-            store.dispatch({ type: SET_IS_LOADING, isLoading: false })
+}
+
+export function updateToy(toy) {
+    storageService.put(toy)
+        .then(() => {
+            store.dispatch({ type: UPDATE_TOY, toy })
         })
+        .catch(err => {
+            console.error('Error updating toy:', err)
+        })
+}
+
+export function addToy(toy) {
+    storageService.post(toy)
+        .then((newToy) => {
+            store.dispatch({ type: ADD_TOY, toy: newToy })
+        })
+        .catch(err => {
+            console.error('Error adding toy:', err)
+        })
+}
+
+export function setFilter(filterBy) {
+    store.dispatch({ type: SET_FILTER, filterBy })
 }
