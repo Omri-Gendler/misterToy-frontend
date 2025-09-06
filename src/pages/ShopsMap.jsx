1afe293c66f748236a04345a1873d244e1c58887
omri-gendler <omrig999@gmail.com>
2025-09-06 12:22:44 +0300

    fix: update .gitignore to include .env; adjust styles in ToyList and ShopsMap components for better layout and responsiveness

--- src/pages/ShopsMap.jsx
+++ src/pages/ShopsMap.jsx
@@ -1,7 +1,7 @@
 import { AdvancedMarker, APIProvider, Map } from '@vis.gl/react-google-maps';
 import { AppHeader } from "../cmps/AppHeader";
 
-const API_KEY = 'AIzaSyCTk12UgjiVgYufd8AiyVp_Quz1R-WtThE'
+const API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY
 
 export function ShopsMap() {
     const position = { lat: 53.54992, lng: 10.00678 }
@@ -11,9 +11,12 @@ export function ShopsMap() {
             <AppHeader />
             <h1>Shops Map</h1>
             <APIProvider apiKey={API_KEY}>
-                <Map defaultCenter={position}
+                <Map
+                    defaultCenter={position}
                     defaultZoom={10}
-                    mapId="DEMO_MAP_ID">
+                    mapId="DEMO_MAP_ID"
+                    style={{ width: '100%', height: '500px' }} // <-- Add this line
+                >
                     <AdvancedMarker position={position} />
                 </Map>
             </APIProvider>
