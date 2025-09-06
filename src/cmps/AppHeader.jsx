7b0c86e934cee587945bde71feaacbc5ebb10d21
omri-gendler <omrig999@gmail.com>
2025-09-07 00:09:04 +0300

    Add new logos and update styles for improved layout and design
    - Added logo2.svg and logo5.svg to the public directory.
    - Changed background color of the body to a lighter shade and updated text color to black.
    - Adjusted container styles for better spacing, padding, and shadow effects.
    - Modified AppHeader styles for full width and added background color with shadow.
    - Enhanced ToyList styles with shadow and padding for better visual appeal.
    - Updated AppHeader component to include the new logo image.
    - Commented out unused CSS properties in index.css for cleaner code.

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -2,12 +2,13 @@ import { NavLink } from "react-router-dom"
 import '../assets/style/cmps/AppHeader.css'
 import { UserMsg } from "./UserMsg"
 
+const logo = '/logo2.svg'
 
 export function AppHeader() {
     return (
         <section className="header-container">
             <header>
-                <h1>Mister Toy</h1>
+                <h1><img src={logo} alt="Mister Toy Logo" width={100} height={100} /></h1>
             </header>
             <nav className="app-nav">
                 <NavLink to="/">Home</NavLink>
